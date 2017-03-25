fa82eeb89d491c5303590272d98285dfe9a93344
kaffeMn <mspringenberg@googlemail.com>
2017-03-25 17:53:10 +0100

    added some .pdfs

--- FuPro/Altklausuren/FuProKl----2014.hs
+++ FuPro/Altklausuren/FuProKl----2014.hs
@@ -86,9 +86,9 @@ data Exp    = Const Int
 
 eval1, eval2 :: Exp -> (Char -> Maybe Int) -> Maybe Int
 
---1.
+-- Wie ich es machen wuerde
 
-eval1 exp f = case exp of
+eval1' exp f = case exp of
     (Const i)       -> Just i
     (Var c)         -> f c
     (Negation e)    -> myMult (eval1 e f) (-1)
@@ -102,19 +102,106 @@ eval1 exp f = case exp of
         Nothing  -> (\x -> Nothing)
         (Just i) -> (\x -> Just $ i + x) 
 
---2.
+--
 
-eval2 exp f = do
+eval2' exp f = do
     case exp of
         (Const i)       -> Just i
         (Var c)         -> f c
         (Negation e)    -> myMult (eval1 e f) (-1)
         (Sum e1 e2)     -> (eval1 e1 f) >>= mySum (eval1 e2 f)
         (Product e1 e2) -> (eval1 e1 f) >>= myMult (eval1 e2 f)
-        where
-        myMult may = case may of
-            Nothing  -> (\x -> Nothing)
-            (Just i) -> (\x -> Just $ x * i)
-        mySum may = case may of 
-            Nothing  -> (\x -> Nothing)
-            (Just i) -> (\x -> Just $ i + x) 
+    where
+    myMult may = case may of
+        Nothing  -> (\x -> Nothing)
+        (Just i) -> (\x -> Just $ x * i)
+    mySum may = case may of 
+        Nothing  -> (\x -> Nothing)
+        (Just i) -> (\x -> Just $ i + x) 
+
+--1.
+
+eval1 (Var c)         f = f c
+eval1 (Const i)       f = Just i
+eval1 (Negation e)    f = case (eval1 e f) of
+    Nothing  -> Nothing
+    (Just i) -> Just $ i * (-1)
+eval1 (Sum e1 e2)     f = case (eval1 e1 f) of
+    Nothing -> Nothing
+    (Just n)  -> case (eval1 e2 f) of
+        Nothing  -> Nothing
+        (Just m) -> Just $ n + m
+eval1 (Product e1 e2) f = case (eval1 e1 f) of
+    Nothing -> Nothing
+    (Just n)  -> case (eval1 e2 f) of
+        Nothing  -> Nothing
+        (Just m) -> Just $ n * m
+
+--2.
+
+eval2 = eval2'
+
+
+--A5
+
+data Maze   = Exit
+            | DeadEnd
+            | Branch Maze Maze
+
+data Direction  = LeftD
+                | RightD
+
+type Way = [Direction]
+
+--1.
+
+follow :: Maybe Way -> Maze -> Maybe Maze
+follow way maze = case way of
+    Nothing -> Nothing
+    Just w  -> walkThrough w maze
+    where
+    walkThrough []     maze = Just maze
+    walkThrough (w:ws) maze = case maze of
+        Exit    -> Nothing
+        DeadEnd -> Nothing
+        (Branch l r) -> case w of
+            LeftD  -> walkThrough ws l
+            RightD -> walkThrough ws r
+
+--2.
+
+shortestPaths :: Maybe Way -> Maybe Way -> Maybe Way
+shortestPaths Nothing   Nothing    = Nothing
+shortestPaths p         Nothing    = p
+shortestPaths Nothing   p          = p
+shortestPaths (Just p1) (Just p2)  
+    = if shorter p1 p2 
+        then (Just p1) 
+        else (Just p2)
+    where
+    shorter [] _          = True
+    shorter _  []         = False
+    shorter (a:as) (b:bs) = shorter as bs
+
+--3.
+
+shortest :: Maze -> Maybe Way
+shortest = shortestHelp []
+
+shortestHelp :: Way -> Maze -> Maybe Way
+shortestHelp w m = case m of 
+    DeadEnd     -> Nothing
+    Exit        -> Just $ reverse w
+    Branch l r  -> do
+        let lW = shortestHelp (LeftD:w) l
+        let rW = shortestHelp (RightD:w) r
+        if isShorter lW rW then lW else rW
+    where
+    isShorter Nothing    Nothing    = True
+    isShorter _          Nothing    = True
+    isShorter Nothing    _          = False
+    isShorter (Just(as)) (Just(bs)) = fastShort as bs
+    fastShort []     [] = True
+    fastShort _      [] = False
+    fastShort []     _  = True
+    fastShort (a:as) (b:bs) = fastShort as bs
